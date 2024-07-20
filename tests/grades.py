35b8b0af944055abf77bfbc15a1520c5faf8db1b
Nithik-09 <110321923+Nithik-09@users.noreply.github.com>
2024-07-20 13:26:44 +0530

    Create grades.py

---
+++ tests/grades.py
@@ -0,0 +1,122 @@
+from core.models.assignments import AssignmentStateEnum, GradeEnum
+
+
+def test_grade_assignment_draft_assignment(client, h_principal):
+    """
+    failure case: If an assignment is in Draft state, it cannot be graded by principal
+    """
+    response = client.post(
+        '/principal/assignments/grade',
+        json={
+            'id': 5,
+            'grade': GradeEnum.A.value
+        },
+        headers=h_principal
+    )
+
+    assert response.status_code == 400
+
+def test_grade_assignment(client, h_principal):
+    response = client.post(
+        '/principal/assignments/grade',
+        json={
+            'id': 4,
+            'grade': GradeEnum.C.value
+        },
+        headers=h_principal
+    )
+
+    assert response.status_code == 200
+
+    assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
+    assert response.json['data']['grade'] == GradeEnum.C
+
+
+def test_regrade_assignment(client, h_principal):
+    response = client.post(
+        '/principal/assignments/grade',
+        json={
+            'id': 4,
+            'grade': GradeEnum.B.value
+        },
+        headers=h_principal
+    )
+
+    assert response.status_code == 200
+
+def test_grade_assignment_cross(client, h_teacher_2):
+    """
+    failure case: assignment 1 was submitted to teacher 1 and not teacher 2
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_2,
+        json={
+            "id": 1,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 400
+    data = response.json
+
+    assert data['error'] == 'FyleError'
+
+
+def test_grade_assignment_bad_grade(client, h_teacher_1):
+    """
+    failure case: API should allow only grades available in enum
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1,
+        json={
+            "id": 1,
+            "grade": "AB"
+        }
+    )
+
+    assert response.status_code == 400
+    data = response.json
+
+    assert data['error'] == 'ValidationError'
+
+
+def test_grade_assignment_bad_assignment(client, h_teacher_1):
+    """
+    failure case: If an assignment does not exists check and throw 404
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1,
+        json={
+            "id": 100000,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 404
+    data = response.json
+
+    assert data['error'] == 'FyleError'
+
+
+def test_grade_assignment_draft_assignment(client, h_teacher_1):
+    """
+    failure case: only a submitted assignment can be graded
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1
+        , json={
+            "id": 2,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 400
+    data = response.json
+
+    assert data['error'] == 'FyleError'
+    assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
+    assert response.json['data']['grade'] == GradeEnum.B
